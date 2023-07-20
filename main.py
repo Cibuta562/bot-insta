822858f885431e985f5cc29dcf2aee6619096675
Irene <ireneoli2130@gmail.com>
2023-07-20 08:27:33 -0700

    sobolan

--- main.py
+++ main.py
@@ -1,1 +1,53 @@
 print("Sobolan Coios")
+import pyautogui
+import time
+pyautogui.moveTo(1000, 313, duration=3.4)
+pyautogui.leftClick(1000, 313)
+pyautogui.moveTo(1137, 234, duration=1.4)
+pyautogui.click()
+text_to_type = "#digitalmarketingagency"
+pyautogui.typewrite(text_to_type)
+time.sleep(2.95)
+pyautogui.moveTo(1227, 372, duration=1.8)
+pyautogui.click()
+pyautogui.moveTo(1767, 193, duration=2.3)
+pyautogui.click(1767, 193)
+time.sleep(2.1)
+num_steps = 6
+pause_time = 0.5
+for _ in range(num_steps):
+    pyautogui.scroll(-500)
+    time.sleep(pause_time)
+
+time.sleep(3.2)
+pyautogui.moveTo(1451, 462, duration=1.6)
+pyautogui.click()
+counter = 0
+max_iterations = 2
+
+while counter < max_iterations:
+
+
+     pyautogui.moveTo(1347, 690, duration=2)
+     pyautogui.click()
+     pyautogui.moveTo(1863, 557, duration=2)
+     pyautogui.click()
+     print("Iteration", counter +1)
+     counter += 1
+time.sleep(2.7)
+pyautogui.moveTo(1864, 131, duration=2)
+pyautogui.click()
+pyautogui.moveTo(1000, 335, duration=2)
+pyautogui.click()
+time.sleep(3.8)
+text_to_type = "#socialmediamarketing"
+pyautogui.typewrite(text_to_type)
+pyautogui.moveTo(1214, 405, duration=2)
+pyautogui.click()
+pyautogui.moveTo(1813, 358, duration=3)
+time.sleep(1.89)
+num_steps = 6
+pause_time = 0.5
+for _ in range(num_steps):
+    pyautogui.scroll(500)
+pyautogui.click(1767, 193)
