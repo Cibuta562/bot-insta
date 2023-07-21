7d0a7e17f5dad900dd3b92f1b97d2861ff35ff97
Irene <ireneoli2130@gmail.com>
2023-07-21 10:36:24 -0700

    sobo final

--- main.py
+++ main.py
@@ -9,81 +9,81 @@ import random
 
 ##################################
 
-pyautogui.moveTo(1000, 313, duration=3.4)
-pyautogui.leftClick(1000, 313)
-pyautogui.moveTo(1137, 234, duration=1.4)
-pyautogui.click()
+oki = 0
+ma_iterations = 3
+ok = 0
+while oki < ma_iterations:
+        pyautogui.moveTo(1000, 313, duration=3.4)
+        pyautogui.leftClick(1000, 313)
+        pyautogui.moveTo(1137, 234, duration=1.4)
+        pyautogui.click()
 
-list_of_has = ["#digitalmarketingagency", "#marketingagency", "#digitalmarketing"]
-has_index = random.randint(0,len(list_of_has)-1)
-chosen_has = list_of_has[has_index]
-
-pyautogui.typewrite(chosen_has)
-time.sleep(2.95)
-pyautogui.moveTo(1227, 372, duration=1.8)
-pyautogui.click()
-pyautogui.moveTo(1767, 193, duration=2.3)
-pyautogui.click(1767, 193)
-time.sleep(2.1)
-
-######################
-
-num_steps = 6
-pause_time = 0.5
-
-for i in range(num_steps):
-    pyautogui.scroll(-174)
-    time.sleep(pause_time)
-
-time.sleep(3.2)
-pyautogui.moveTo(1205, 505, duration=1.6)
-pyautogui.click()
-counter = 0
-max_iterations = 20
-
-list_of_coms = ["Keep up the good posts! 📈", "Great post! 👀", "This post is so well made! 🔥"]
-list_of_coms = random.sample(list_of_coms,len(list_of_coms))
-# coms_index = random.randint(0,len(list_of_coms)-1)
-# chosen_coms = list_of_coms[coms_index]
+        list_of_has = ["#digitalmarketingagency", "#marketingagency", "#digitalmarketing"]
+       # list_random_hastags = random.sample(list_of_has, len(list_of_has))
 
 
-while counter < max_iterations:
-     coms_index = counter
-     chosen_coms = list_of_coms[coms_index]
-     time.sleep(3.4)
-     pyautogui.moveTo(1347, 690, duration=2)
-     pyautogui.click()
-     pyautogui.moveTo(1448, 805, duration=1.8)
-     pyautogui.click()
-     time.sleep(1.2)
-     pyautogui.typewrite(chosen_coms)
-     pyautogui.moveTo(1782, 809, duration=1.4)
-     pyautogui.click()
-     pyautogui.moveTo(1863, 557, duration=2)
-     pyautogui.click()
-     print("Iteration", counter +1)
-     counter += 1
-time.sleep(2.7)
-pyautogui.moveTo(1864, 131, duration=2)
-pyautogui.click()
-pyautogui.moveTo(1000, 335, duration=2)
-pyautogui.click()
-time.sleep(3.9)
-pyautogui.typewrite(chosen_has)
-time.sleep(2.7)
-pyautogui.moveTo(1214, 405, duration=2)
-pyautogui.click()
-pyautogui.moveTo(1813, 358, duration=3)
-time.sleep(1)
-
-num_steps = 6
-pause_time = 0.4
+        pyautogui.typewrite( list_of_has[ok])
+        print( list_of_has[ok])
+        ok +=1
 
 
-for i in range(num_steps):
-    pyautogui.scroll(245)
-pyautogui.click()
+        time.sleep(2.95)
+        pyautogui.moveTo(1227, 372, duration=1.8)
+        pyautogui.click()
+        pyautogui.moveTo(1767, 193, duration=2.3)
+        pyautogui.click(1767, 193)
+        time.sleep(2.1)
 
+        ######################
+
+        num_steps = 6
+        pause_time = 0.5
+
+        for i in range(num_steps):
+         pyautogui.scroll(-174)
+         time.sleep(pause_time)
+
+        time.sleep(3.2)
+        pyautogui.moveTo(1205, 505, duration=1.6)
+        pyautogui.click()
+        counter = 0
+        max_iterations = 3
+
+        list_of_coms = ["Keep up the good posts! 📈", "Great post! 👀", "This post is so well made! 🔥"]
+        list_of_coms = random.sample(list_of_coms, len(list_of_coms))
+        # coms_index = random.randint(0,len(list_of_coms)-1)
+        # chosen_coms = list_of_coms[coms_index]
+
+        while counter < max_iterations:
+             coms_index = counter
+             chosen_coms = list_of_coms[coms_index]
+             time.sleep(3.4)
+             pyautogui.moveTo(1347, 690, duration=2)
+             pyautogui.click()
+             pyautogui.moveTo(1448, 805, duration=1.8)
+             pyautogui.click()
+             time.sleep(1.2)
+             pyautogui.typewrite(chosen_coms)
+             pyautogui.moveTo(1782, 809, duration=1.4)
+             pyautogui.click()
+             pyautogui.moveTo(1863, 557, duration=2)
+             pyautogui.click()
+             print("Iteration", counter + 1)
+             counter += 1
+        time.sleep(2.7)
+        pyautogui.moveTo(1864, 131, duration=2)
+        pyautogui.click()
+        time.sleep(1)
+
+        num_steps = 6
+        pause_time = 0.4
+
+        for i in range(num_steps):
+         pyautogui.scroll(245)
+        pyautogui.click()
+
+        print("Iteratio", counter + 1)
+        oki += 1
 
 ####################################
 
